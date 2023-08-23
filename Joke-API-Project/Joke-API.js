fa4087f3b36d28beb2c89621b9c4d3e615911982
Richard Dalrymple <117475164+Tcherin@users.noreply.github.com>
2023-08-23 11:27:33 +0100

    async tutorial notes

--- Joke-API-Project/Joke-API.js
+++ Joke-API-Project/Joke-API.js
@@ -1,18 +1,15 @@
+const url = "https://official-joke-api.appspot.com/jokes/programming/random";
 const btn = document.querySelector(".joke-btn");
 const joke = document.querySelector(".joke-content");
 
-btn.addEventListener("click", getJoke);
-
-getJoke();
+btn.addEventListener("click", () => {
+  getJoke();
+});
 
 const getJoke = async () => {
-  const response = await fetch(
-    "https://official-joke-api.appspot.com/jokes/programming/random"
-  );
+  const response = await fetch(url);
   const data = await response.json();
-  return data;
+  joke.textContent = data[0].setup + " " + data[0].punchline;
 };
 
-getJoke().then((data) => {
-  console.log(data);
-});
+getJoke();
